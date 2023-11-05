e938bb599c430adb1c7400fe91a4ed9db323b0ae
Sergio Piña <sepi_147@hotmail.com>
2023-11-05 11:21:42 -0500

    Agrego configuracion inicial del proyecto

---
+++ src/test/java/com/challenge/bankapi/BankApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.bankapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
